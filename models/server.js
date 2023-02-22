8b06dc78207ffbcf9ce6bb075340c8d2d8babd18
Marvin Mazariegos <mmazariegos-2021338@kinal.edu.gt>
2023-02-22 17:20:17 -0600

    Subiendo Repositorio

---
+++ models/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const {dbConection} = require('../database/config');
+
+class Server{
+    constructor(){
+        //variables de configuración
+        this.app = express();
+        this.port = process.env.PORT;
+        this.categoriaPath = '/api/categorias'
+        this.productoPath = '/api/productos'
+        this.usuarioPath = '/api/usuarios'
+
+    
+        //Conectar a la base de datos
+        this.conectarDB();
+
+        //middleware
+        this.middlewares();
+
+        //rutas de mi app
+
+        this.routes();
+    
+    }
+
+    //Metodo de conexion a mongo
+    async conectarDB(){
+        await dbConection();
+    }
+
+    middlewares(){
+
+        //cors
+        this.app.use(cors());
+        this.app.use(express.static('public'));
+         //Lectura y parseo del body
+         this.app.use( express.json() );
+    }
+
+    routes(){
+        this.app.use(this.categoriaPath, require('../routes/categoria'));
+        this.app.use(this.productoPath, require('../routes/producto'));
+        this.app.use(this.usuarioPath, require('../routes/usuario'));
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en puerto ${this.port}`);
+        })
+    }
+}
+module.exports = Server;
